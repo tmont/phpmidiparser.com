b474c836a57775925359120972de23613de4672f
tmont <tommy.mont@gmail.com>
2014-01-22 13:13:06 -0800

    added virtual host

--- provision.sh
+++ provision.sh
@@ -6,3 +6,6 @@ apt-get install -y php5 php5-fpm nginx mysql-server
 
 sed -i 's/^bind-address/#bind-address/' /etc/mysql/my.cnf
 restart mysql
+
+cp /vagrant/provision/phpmidiparser.com.conf /etc/nginx/sites-enabled
+/etc/init.d/nginx reload
